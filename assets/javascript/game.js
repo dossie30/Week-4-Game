38ef9786043b2e5204410d4152f39bd7e9583925
Dawn Tossie <dawn.tossie@gmail.com>
2018-02-27 14:39:49 -0500

    <clicked image working>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,16 +10,23 @@ $(document).ready(function () {
 
   // Create variables for game
   var randomNumber = "";
+  var counter = 0;
 
   // Generate a new random number between 19 and 120.
   var randomN = Math.floor(Math.random() * 121) + 18;
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $(".img-fluid").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
 
   // Assign random number to string
   $random.textContent = randomN;
   console.log($random);
 
-  // Append randomNumber to window
-  //$("#random").html(randomNumber);
- // console.log(randomNumber);
+  
 
 })
